faf5fb7c35b64b522b0b32b700c16ab551f7d02a
Anas Ansari <itsofficial177@gmail.com>
2023-03-21 12:22:40 +0530

    first

---
+++ src/scenes/calender/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calendar = () => {
+  return <div>Calender </div>;
+};
+
+export default Calendar;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box></Box>
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  );
+};
+
+export default Dashboard;
